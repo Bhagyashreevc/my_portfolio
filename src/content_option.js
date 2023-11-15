3b78da8f743d22e766ba6bca3282990ac29da5fd
Bhagyashreevc <123979229+Bhagyashreevc@users.noreply.github.com>
2023-11-15 18:23:46 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,107 @@
+const logotext = "Bhagyashree V C's Portfolio";
+const meta = {
+    title: "Bhagyashree V C",
+    description: "Student seeking an opportunity for an internship position in a dynamic organization to apply skills and gain valuable experience.",
+};
+
+const introdata = {
+    title: "Bhagyashree V C",
+    animated: {
+        first: "Dancer",
+        second: "Artist",
+        third: "Writer",
+    },
+    description: "Aspiring student with a passion for dance, art, and communication. Proficient in Python.",
+    your_img_url: "https://i.ibb.co/56dLWtD/6240249763970792759-121.jpg",
+
+
+};
+
+const dataabout = {
+    title: "About Me",
+    aboutme: "I'm a student at New Horizon College of Engineering with a strong academic background and a passion for dance, art, communication, and writing.",
+};
+
+const worktimeline = [{
+        jobtitle: "New Horizon College of Engineering",
+        where: "Student",
+        date: "11/2021 - 05/2025",
+    },
+    {
+        jobtitle: "Global Integrated PU College, Hubli",
+        where: "Pre University",
+        date: "05/2019 - 05/2021",
+    },
+    {
+        jobtitle: "K. L. E. Society's English Medium School",
+        where: "Grade 10",
+        date: "97",
+    },
+];
+
+const skills = [{
+        name: "Dance",
+        value: 100,
+    },
+    {
+        name: "Art",
+        value: 80,
+    },
+    {
+        name: "Communication",
+        value: 90,
+    },
+    {
+        name: "Writing",
+        value: 80,
+    },
+    {
+        name: "Basics of Python",
+        value: 70,
+    },
+];
+
+const services = [
+    {
+        title: "Winner of Hackathon at New Horizon College of Engineering",
+        description: "Recognized for technical skills and innovation."
+    },
+    {
+        title: "Winner of Ideathon at New Horizon College of Engineering",
+        description: "Acknowledgment of creative thinking and problem-solving."
+    },
+    {
+        title: "Speech competition - Govt. of India, Ministry of Telecommunication",
+        description: "Acknowledgment for effective communication skills."
+    },
+    {
+        title: "Co-author of a book in Anthology",
+        description: "An achievement in literature and creative writing."
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "bhagyachikkeri@gmail.com",
+    YOUR_FONE: "8#########3",
+    description: "As a student with a passion for dance, art, and communication, I bring a unique blend of creativity and technical skills. Winner of hackathons and ideathons, and co-author of a book, I am ready for dynamic opportunities.",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/bhagyashreevc",
+    // Update other social profiles as needed
+};
+
+export {
+    meta,
+    dataabout,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
